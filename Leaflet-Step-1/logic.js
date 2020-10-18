b2b5258e004d4f9e9900c2d7916acfb77588e1c9
Julia Alsop <64661590+JuliaAlsop@users.noreply.github.com>
2020-10-17 23:59:04 -0400

    addition of earthquake data and beginning of circles

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -5,7 +5,10 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_da
 d3.json(queryUrl, function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
   createFeatures(data.features);
+  
 });
+
+
 
 function createFeatures(earthquakeData) {
 
@@ -28,7 +31,7 @@ function createFeatures(earthquakeData) {
 
 function createMap(earthquakes) {
 
-  // Define streetmap and darkmap layers
+  // Define streetmap layer & required attribution for copyright
   var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -37,38 +40,37 @@ function createMap(earthquakes) {
     id: "mapbox/streets-v11",
     accessToken: API_KEY
   });
-
-  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox/dark-v10",
-    accessToken: API_KEY
-  });
-
+  var circle = L.circle([39.11, -105], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(earthquakes);
   // Define a baseMaps object to hold our base layers
-  var baseMaps = {
-    "Street Map": streetmap,
-    "Dark Map": darkmap
-  };
+  //var baseMaps = {
+    //"Street Map": streetmap,
+    //"Dark Map": darkmap
+  //};
 
   // Create overlay object to hold our overlay layer
   var overlayMaps = {
     Earthquakes: earthquakes
   };
 
-  // Create our map, giving it the streetmap and earthquakes layers to display on load
+  // Create map, giving it layers to display 
+  // Map center on Nebraska and zoomed out for entire U.S. view
   var myMap = L.map("map", {
     center: [
-      37.09, -95.71
+      41.49, -99.90
     ],
-    zoom: 5,
+    zoom: 3.5,
     layers: [streetmap, earthquakes]
   });
 
   // Create a layer control
   // Pass in our baseMaps and overlayMaps
   // Add the layer control to the map
-  L.control.layers(baseMaps, overlayMaps, {
+  L.control.layers(overlayMaps, {
     collapsed: false
   }).addTo(myMap);
 }
